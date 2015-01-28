a39b27d0b20e6ff214ee977992f679d72f643da6
descartez <rvanstong@gmail.com>
2015-01-28 14:06:07 -0800

    Adds angular to flipbook page, begins 'flipping' functionality. Changes animations so fadeIn effect only applies to homepage.

--- angularflipbook/flipbook.js
+++ angularflipbook/flipbook.js
@@ -1,1 +1,11 @@
 var flipApp = angular.module('flipApp', [])
+flipApp.controller('flipCtrl', [function(){
+  console.log('controller has loaded!');
+  var self = this;
+  self.panels = ['panel 1', 'panel 2', 'panel 3'];
+  self.currentPanel = self.panels[0];
+  self.nextPanel = function() {
+    self.currentPanel = self.panels[1];
+    console.log('panels should change!')
+  }
+}])
