814c09aca40371bdf091e531dcb84b545e8866cd
descartez <rvanstong@gmail.com>
2015-01-28 15:29:03 -0800

    adds supes and building assets for flipbook

--- angularflipbook/flipbook.js
+++ angularflipbook/flipbook.js
@@ -2,10 +2,21 @@ var flipApp = angular.module('flipApp', [])
 flipApp.controller('flipCtrl', [function(){
   console.log('controller has loaded!');
   var self = this;
-  self.panels = ['panel 1', 'panel 2', 'panel 3'];
+  self.panels = ['buildings001.png', 'buildings002.png', 'buildings003.png'];
+  var panelIndex = 0;
   self.currentPanel = self.panels[0];
+
+
   self.nextPanel = function() {
-    self.currentPanel = self.panels[1];
-    console.log('panels should change!')
+    console.log(self.panels[panelIndex]);
+    if (panelIndex < self.panels.length-1) {
+      panelIndex+=1;
+      self.currentPanel = self.panels[panelIndex]
+    }else if (panelIndex === self.panels.length-1){
+      panelIndex=0;
+      self.currentPanel = self.panels[panelIndex]
+    };
+    console.log('panels should change!');
+    console.log(self.panels[panelIndex]);
   }
 }])
