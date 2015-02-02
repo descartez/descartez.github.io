20608030bfc4fed10f9bc6f591ad7cdea2103aed
descartez <rvanstong@gmail.com>
2015-02-01 22:27:23 -0800

    adds further styling, final projects, experience, education tiles

--- patreon-resume/patreon_resume.js
+++ patreon-resume/patreon_resume.js
@@ -7,7 +7,6 @@ app.controller('InfoCtrl',[function(){
     { title: 'About', content: 'I enjoy the technical and social - science, art, psychology, sociology - which enhance my creative skills and art. I love working in teams, making products which communicate their use and their purpose well. Looking for work that is both creative and challenging.', picture: '../img/profile.png', link:'', tag: 'about'},
     { title: 'Skills', content: 'JavaScript, AngularJS, Ruby, HTML/CSS, Sinatra, the ability to pick things up as quickly as Spidey catches crooks.', picture: 'img/spider-man-60s-cartoon.jpg', link:'', tag: 'skills' },
     { title: 'Why Dev?', content: 'After graduating from college, I found myself with an excellent degree but no hard skills. I found work where I could, leveraging my design instincts and work ethic wherever possible. Then I found Dev Bootcamp. I pursued my own education, gathering hard skills in Web Dev. Now I go on, but not unarmed.', picture: 'img/dangerous-to-go.jpg', link:'', tag: 'why-dev' },
-    // { title: 'Resumé', content: 'Some people like paper things. I get that. Here\'s a paper with my info on it!', picture: '', link:'' },
   ];
 
   self.education = [
@@ -17,18 +16,16 @@ app.controller('InfoCtrl',[function(){
   ];
 
   self.experience = [
-    { company: '', position: '', timePeriod: '' },
-    { company: '', position: '', timePeriod: '' },
-    { company: '', position: '', timePeriod: '' },
-    { company: '', position: '', timePeriod: '' },
-    { company: '', position: '', timePeriod: '' },
+    { company: 'Dev Bootcamp', position: 'Web Dev Student', timePeriod: 'Aug 2014-Jan 2015' },
+    { company: 'PSAV Presentaions', position: 'Presentation Tech, Core Team member', timePeriod: 'April-Aug 2014' },
+    { company: 'Cheetah Conservation Fund', position: 'Advertising Rep, Graphic Designer (volunteer)', timePeriod: 'Feb 2014' },
+    { company: 'Sur La Table', position: 'Retail Associate, Team Member', timePeriod: 'Nov 2013-Feb 2014' },
   ];
 
   self.projects = [
-    { name: 'Code Crush', description: 'Interactive typing game built in one week as final project for Dev Boot Camp. Created to increase skill and speed in typing symbols and code.', contribution: 'Assets, Front End, Site Design, UX', tech: 'Phaser.io, JavaScript, MongoDB, Express, AngularJS, Node', link:'http://codecrush.herokuapp.com/#/home', shortLink: 'codecrush.herokuapp.com'},
-    { name: 'Invisible Kingdom', description: 'Story generator which ties randomly generated content to geolocation coordinates.', contribution: 'Content, Functionality, UX', tech: 'Sinatra, ActiveRecord, jQuery, AJAX', link:'http://invisiblekingdom.herokuapp.com/', shortLink: 'invisiblekingdom.herokuapp.com'},
-    { name: '', description: '', contribution: '', tech: '', link:'', shortLink: ''},
-    { name: '', description: '', contribution: '', tech: '', link:'', shortLink: ''},
+    { name: 'Code Crush', description: 'Interactive typing game built in one week as final project for Dev Boot Camp. Created to increase skill and speed in typing symbols and code.', contribution: 'Assets, Front End, Site Design, UX', tech: 'Phaser.io, JavaScript, MongoDB, Express, AngularJS, Node', link:'http://codecrush.herokuapp.com/#/home', repoLink: 'https://github.com/descartez/code-crush'},
+    { name: 'Invisible Kingdom', description: 'Story generator which ties randomly generated content to geolocation coordinates.', contribution: 'Content, Functionality, UX', tech: 'Sinatra, ActiveRecord, jQuery, AJAX', link:'http://invisiblekingdom.herokuapp.com/', repoLink: 'https://github.com/descartez/invisiblekingdom'},
+    { name: 'algorithm.ic', description: 'A collection of popular algorithms used in coding interviews, and their solutions.', contribution: 'Content', tech: 'n/a', link:'https://github.com/descartez/algorithm.ic', repoLink: 'github.com/descartez/algorithm.ic'},
   ];
 
 }]);
